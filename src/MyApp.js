3e840d709e7782166344e769faaa2bbed8f0c0dc
ARES-79 <89227872+ARES-79@users.noreply.github.com>
2024-01-24 18:50:50 -0800

    connected to backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table';
 import Form from './Form';
    
@@ -12,8 +13,45 @@ function MyApp() {
             setCharacters(updated);
         }
 
+    // function updateList(person) {
+    //     setCharacters([...characters, person]);
+    // }
+
+    async function fetchAll(){
+        try {
+            const response = await axios.get('http://localhost:5000/users');
+            return response.data.users_list;    
+        }
+        catch (error){
+            //We're not handling errors. Just logging into the console.
+            console.log(error);
+            return false;         
+        }
+    }
+
+    useEffect(() => {
+        fetchAll().then( result => {
+            if (result)
+                setCharacters(result);
+        });
+    }, [] );
+
+    async function makePostCall(person){
+        try {
+        const response = await axios.post('http://localhost:5000/users', person);
+        return response;
+        }
+        catch (error) {
+        console.log(error);
+        return false;
+        }
+       }
+
     function updateList(person) {
-        setCharacters([...characters, person]);
+        makePostCall(person).then( result => {
+        if (result && result.status === 201)
+        setCharacters([...characters, person] );
+        });
     }
 
     return (
